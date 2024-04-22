558f518c357cf4c9f60fbffcaaa4eb37fb286ba6
daffasatrianegara <daffasatria036@gmail.com>
2024-04-22 19:31:58 +0700

    feat : layouting UI

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row bg-primary p-4 text-white">
+            <p class="col h2 w-25 m-0">Navbar</p>
+            <div class="col d-flex flex-row gap-4">
+                <p class="h6 mt-3 m-0">Beranda</p>
+                <p class="h6 mt-3 m-0">Layanan Kami</p>
+                <p class="h6 mt-3 m-0">Informasi Acara</p>
+                <p class="h6 mt-3 m-0">Tentang Kami</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row bg-secondary p-4 text-white">
+            <p class="h2 text-center py-5">Header</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row bg-light text-dark p-4">
+            <p class="h3 text-center">Content 1</p>
+        </div>
+        <div class="row text-light">
+            <div class="col bg-warning p-4">
+                <p class="h3 text-center">Content 2.1</p>
+            </div>
+            <div class="col bg-dark p-4">
+                <p class="h3 text-center">Content 2.2</p>
+            </div>
+        </div>
+        <div class="row bg-light p-4 text-dark">
+            <p class="h3 text-center">Content 3</p>
+        </div>
+        <div class="row bg-info p-4 text-white">
+            <div class="position-relative">
+                <p class="h3 text-center">Content 4</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row bg-danger p-5 text-white">
+            <p class="h3 text-center">Footer</p>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
